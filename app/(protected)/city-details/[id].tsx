73239b4efbe0f80b23d18c8d701c58c2bc30a8a6
Gabriel J. <gabrieldejesus16gs@gmail.com>
2026-08-07 12:42:53 -0300

    feat: initial boilerplate screen cityDetails

--- app/(protected)/city-details/[id].tsx
+++ app/(protected)/city-details/[id].tsx
@@ -1,15 +1,16 @@
+import { Screen } from "@/src/components/Screen";
+import { CityDetailsHeader } from "@/src/containers/CityDetailsHeader";
 import { useLocalSearchParams, useRouter } from "expo-router";
-import { Text, View } from "react-native";
 
 export default function CityDetails() {
   const router = useRouter();
   const { id, name } = useLocalSearchParams();
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text onPress={router.back}>
-        City Details: {id} {name}
-      </Text>
-    </View>
+    <Screen>
+      <CityDetailsHeader
+        cityName={name}
+      />
+    </Screen>
   );
 }
---
+++ src/containers/CityDetailsHeader.tsx
@@ -0,0 +1,13 @@
+import { Box } from "../components/Box";
+import { Text } from "../components/Text";
+
+type CityDetailsHeaderProps = {
+  cityName: string;
+}
+export function CityDetailsHeader() {
+  return (
+    <Box>
+      <Text>CityDetailsHeader</Text>
+    </Box>
+  )
+}
