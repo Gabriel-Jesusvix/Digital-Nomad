11a5db6e556d805059a899fb74e8c20728cb8fdc
Gabriel J. <gabrieldejesus16gs@gmail.com>
2026-08-10 08:15:11 -0300

    feat: create structure accordion

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,10 +1,15 @@
 import { PropsWithChildren } from "react";
+import { ScrollView, View } from "react-native";
 import { Box, BoxProps } from "./Box";
 
-export function Screen({ children, ...boxProps }: PropsWithChildren & BoxProps) {
+export function Screen({ children, scrollable = false, ...boxProps }: PropsWithChildren & BoxProps & { scrollable?: boolean }) {
+
+  const Container = scrollable ? ScrollView : View
   return (
     <Box flex={1} backgroundColor="background" paddingHorizontal="padding" {...boxProps}>
-      {children}
+      <Container>
+        {children}
+      </Container>
     </Box>
   )
 }
